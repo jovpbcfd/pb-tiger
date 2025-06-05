bc5911381863f4b6d2d6b1ff315ad891aa42ab8e
Jovan Lanutan <jovan.lanutan@cwti.local>
2025-06-05 11:05:40 +0800

    Add default background in main

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        <main>{children}</main>
+        <main className="bg-[#252B73]">{children}</main>
         <Footer />
       </body>
     </html>
